105066cc51a6c49bf16cddd8a348541b550c25cb
Chav Sumalinog <chaviotics@gmail.com>
2021-02-15 15:55:17 +0800

    Added database using CSV.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
+
 app = Flask(__name__)
 
 @app.route('/') 
@@ -14,14 +16,25 @@ def write_to_file(data):
         email = data['email']
         subject = data['subject']
         message = data['message']
-        file = database.write(f"\n{email}, {subject}, {message}")
+        file = database.write(f"\n{email},{subject},{message}")
+
+def write_to_csv(data):
+    with open('.\database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict() # getting all data as a dictionary
-        write_to_file(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict() # getting all data as a dictionary
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database. :('
     else:
         return 'Something went wrong. Please try again.'
 
