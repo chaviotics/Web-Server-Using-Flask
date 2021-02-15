9b8fba60b2febe2e3d37cfcb5f667ef45b1ed016
Chav Sumalinog <chaviotics@gmail.com>
2021-02-15 15:34:39 +0800

    Added database.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/') 
@@ -9,14 +9,23 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('.\database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f"\n{email}, {subject}, {message}")
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict() # getting all data as a dictionary
-        print(data)
-        return 'Form submitted. Thank you!'
+        write_to_file(data)
+        return redirect('/thankyou.html')
     else:
         return 'Something went wrong. Please try again.'
+
+# Favicon does not work.
 
 
 
