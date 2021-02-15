92030d0701b553a4beece8b794a5564502fa0a56
Chav Sumalinog <chaviotics@gmail.com>
2021-02-15 13:35:00 +0800

    Shortened the @app.route() code for dynamic changes per html file, fixed bugs per .html file and removed components.html

--- server.py
+++ server.py
@@ -17,20 +17,25 @@ def my_home():
 
 # find a way for same .html file but different routes.
 
-@app.route('/works.html') 
-def works():
-    return render_template('works.html')
+@app.route('/<string:page_name>') 
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/about.html') 
-def about_me():
-    return render_template('about.html')
 
-@app.route('/contact.html') 
-def contact():
-    return render_template('contact.html')
+# @app.route('/works.html') 
+# def works():
+#     return render_template('works.html')
 
-@app.route('/components.html') 
-def components():
-    return render_template('components.html')
+# @app.route('/about.html') 
+# def about_me():
+#     return render_template('about.html')
+
+# @app.route('/contact.html') 
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/components.html') 
+# def components():
+#     return render_template('components.html')
 
 
