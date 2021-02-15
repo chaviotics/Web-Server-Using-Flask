a41f7bf4ebe7d9fa2cf5aaee83375aa239d29f2a
Chav Sumalinog <chaviotics@gmail.com>
2021-02-15 14:16:20 +0800

    Added submit form for contacts, started thankyou.html.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/') 
@@ -9,6 +9,14 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict() # getting all data as a dictionary
+        print(data)
+        return 'Form submitted. Thank you!'
+    else:
+        return 'Something went wrong. Please try again.'
 
 
 
